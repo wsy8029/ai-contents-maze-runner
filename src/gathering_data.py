aaeda2a0923b6ca576e07ad12751e81bfbee3b65
wsy8029 <wsy8029@gmail.com>
2020-09-02 17:45:43 +0900

    Implement data gathering at Jupyter Notebook

--- src/gathering_data.py
+++ src/gathering_data.py
@@ -1,26 +1,15 @@
+from modicar import Car
 import modi
-from IPython.display import clear_output
-import time
 
+car = Car()
 bundle = modi.MODI()
 mot = bundle.motors[0]
 dial = bundle.dials[0]
+btn = bundle.buttons[0]
 
-base_speed = -20, 20
-
+print("press button to start")
 while True:
-    mot.speed = base_speed
-    degree = (dial.degree - 50) // 1
-    print(degree)
-
-    if degree <= 0:
-        mod_speed = base_speed[0], (base_speed[1] + abs(degree))
-        mot.speed = mod_speed
-        print(mod_speed)
+    if btn.toggled:
+        car.run(mot, dial.degree)
     else:
-        mod_speed = (base_speed[0] - abs(degree)), base_speed[1]
-        mot.speed = mod_speed
-        print(mod_speed)
-
-    clear_output(wait=True)
-    # time.sleep(0.001)
+        mot.speed = 0,0
--- src/modicar.py
+++ src/modicar.py
@@ -1,14 +1,19 @@
-
+import time
+import pandas as pd
 
 class Car(object):
     def __init__(self):
-        self.base_speed = -20, 20
-        pass
+        self.base_speed = -25, 25
+        self.data = []
+        
 
-    def run(self, mot, degree_original):
+    def run(self, mot, ir1, ir2, degree_original):
+        
+        
         mot.speed = self.base_speed
-        degree = (degree_original - 50)//1
+        degree = ((degree_original - 50)*1.5)//1
         print(degree)
+        self.data.append([ir1.proximity, ir2.proximity, degree_original])
 
         if degree <= 0:
             mod_speed = self.base_speed[0], (self.base_speed[1] + abs(degree))
@@ -18,4 +23,9 @@ class Car(object):
             mod_speed = (self.base_speed[0] - abs(degree)), self.base_speed[1]
             mot.speed = mod_speed
             print(mod_speed)
-
+            
+    def save(self):
+        df = pd.DataFrame(self.data, columns=['ir1', 'ir2', 'degree']) 
+        self.path = '../data/data.csv'
+        df.to_csv(self.path, sep=',')
+        
